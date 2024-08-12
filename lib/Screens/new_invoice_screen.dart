341891ce90d9db2850f9fdc4f3ef127f995f4860
Abhiram T Sabu <sandweepsabu@gmail.com>
2024-08-12 16:06:52 +0530

    added barrel file in widgets and added icons for new invoice screen

---
+++ lib/Screens/new_invoice_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:quick_bill/widgets/custom_app_bar.dart';
+
+class NewInvoiceScreen extends StatelessWidget {
+  const NewInvoiceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: customAppBar(),
+    );
+  }
+}
--- lib/constants/string_constants.dart
+++ lib/constants/string_constants.dart
@@ -3,3 +3,10 @@ String splashBg = "assets/SplashBg.png";
 //HomeScreen
 String squareLogo = "assets/square.svg";
 String addPageIcon = "assets/addPageIcon.png";
+//NewInvoiceScreen
+String addAccount = "assets/addAccount.png";
+String addToCart = "assets/addToCart.png";
+String greenTick = "assets/greenTick.png";
+String org = "assets/Org.png";
+String payment = "assets/payment.png";
+String signature = "assets/signature.png";
